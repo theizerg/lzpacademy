cc0f8f0aba1e1d2c9e336f86ccaf200d0236a45c
Techno Systems <technosystem@gmail.com>
2021-11-16 14:05:18 -0400

    Fix rutas home

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/inicio', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
@@ -24,7 +24,7 @@ Route::middleware(['auth',])->group(function () {
 #############################################################################################
 ##################  Administación del sistema   #############################################
 #############################################################################################
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 #############################################################################################
 ##################  Administación de Usuarios   #############################################
